53606cb0fd3066a283f9afe964e8707832f1dc57
C3Pool <hkfoan@gmail.com>
2023-04-06 15:31:56 +0800

    Fixed empty pass case

--- cat_gminer/h-run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export GPU_MAX_HEAP_SIZE=100
-export GPU_MAX_ALLOC_PERCENT=100
-export GPU_SINGLE_ALLOC_PERCENT=100
-
-DEBIAN_FRONTEND=noninteractive apt install -y nodejs
-eval ./mm.js /run/hive/miners/custom/cat_gminer_mm.json $(< ./cat_gminer.conf)
--- cat_xmrig/h-run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-hugepages -rx
-
-./xmrig
